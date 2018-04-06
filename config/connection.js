e6013c10856711bb1f74641df35725073f2ddb47
Jennifer <white.jenniferann@gmail.com>
2018-04-05 20:30:59 -0500

    deploy push

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
+var connection;
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: 'tyduzbv3ggpf15sx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        user: '	yhpcbh89m5aggutt',
-        password: 'oi64xf31zj051aer',
-        database: 'hdh665oyyajc48hf'
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "burgers_db"
     });
 };
 // Make connection.
